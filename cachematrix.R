34eef121ed9a5152d6a4c916c133a80f56971899
zgoodata <zgoodata@users.noreply.github.com>
2016-04-09 17:45:40 +0200

    Implement matrix object that can cache its inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Lexical Scoping
+## Use lexical scoping to preserve the state of a matrix object
+## Cache the inverse of a matrix
 
-## Write a short comment describing this function
 
+# Creates a matrix object that can hold its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedsolve <- NULL
+    set <- function(y) {
+        x <<- y
+        cachedsolve <<- NULL
+    }
+    get <- function() {
+        x
+    }
+    setsolve <- function(y) {
+        cachedsolve <<- y
+    }
+    getsolve <- function() {
+        cachedsolve
+    }
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+# Returns the cached inverse if available
+# Otherwise computes then returns the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    solveval <- x$getsolve()
+    if (!is.null(solveval)) {
+        message("getting cached data")
+        return(solveval)
+    }
+    data <- x$get()
+    solveval <- solve(data, ...)
+    x$setsolve(solveval)
+    solveval
 }
